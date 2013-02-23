ef29ad22a8c5858b5c10be3bd16731e669704c3d
Q <q@q-continuum.net>
2013-02-23 06:16:10 -0700

    Really use 4 retries

--- itunes_search.py
+++ itunes_search.py
@@ -8,6 +8,8 @@ BASE_URL = 'https://itunes.apple.com/search'
 
 def search(term, country='US', media='all', entity=None, attribute=None, limit=50, lang='en_us', explicit='Yes'):
  params = {k:v for k, v in locals().iteritems() if v is not None}
- response = requests.get(BASE_URL, params=params)
+ session = requests.session()
+ session.adapters['https://'].max_retries = 4
+ response = session.get(BASE_URL, params=params)
  response.raise_for_status()
  return response.json()['results']
