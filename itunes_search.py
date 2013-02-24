77f864fccc414a39a5b40b91e27dcea297fcc751
Q <q@q-continuum.net>
2013-02-24 03:01:36 -0700

    turned off verification of certs (ugly) and upped max retries (necessary)

--- itunes_search.py
+++ itunes_search.py
@@ -9,7 +9,7 @@ BASE_URL = 'https://itunes.apple.com/search'
 def search(term, country='US', media='all', entity=None, attribute=None, limit=50, lang='en_us', explicit='Yes'):
  params = {k:v for k, v in locals().iteritems() if v is not None}
  session = requests.session()
- session.adapters['https://'].max_retries = 4
- response = session.get(BASE_URL, params=params)
+ session.adapters['https://'].max_retries = 5
+ response = session.get(BASE_URL, params=params, verify=False)
  response.raise_for_status()
  return response.json()['results']
