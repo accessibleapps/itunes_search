e056e17c6ef4d45c0276067d6a4e2ba7cc5cbc47
Q <q@q-continuum.net>
2013-03-15 10:04:54 -0600

    Don't depend on Requests

--- itunes_search.py
+++ itunes_search.py
@@ -1,15 +1,16 @@
-import requests
+import json
+import urllib
 
-__version__ = 0.1
+__version__ = 0.2
 __author__ = 'Christopher Toth'
-__doc__ = """Simple wrapper over the iTunes search API, using Requests"""
+__doc__ = """Simple wrapper over the iTunes search API"""
 
 BASE_URL = 'https://itunes.apple.com/search'
 
 def search(term, country='US', media='all', entity=None, attribute=None, limit=50, lang='en_us', explicit='Yes'):
  params = {k:v for k, v in locals().iteritems() if v is not None}
- session = requests.session()
- session.adapters['https://'].max_retries = 5
- response = session.get(BASE_URL, params=params, verify=False)
- response.raise_for_status()
- return response.json()['results']
+ url = '%s?%s' % (BASE_URL, urllib.urlencode(params))
+ response = urllib.urlopen(url).read()
+ return json.loads(response)['results']
+
+
