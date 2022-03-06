f8d981704f37b5dab66f4b9e97e8a7504344e195
Christopher Toth <q.alpha@gmail.com>
2022-03-05 19:16:55 -0700

    Minor code cleanups
    - Updated use of iteritems to items
    - use response.json() instead of the json package

--- itunes_search.py
+++ itunes_search.py
@@ -1,13 +1,12 @@
-import json
 import requests
 
 
 BASE_URL = 'https://itunes.apple.com/search'
 
 def search(term, country='US', media='all', entity=None, attribute=None, limit=50, lang='en_us', explicit='Yes'):
- params = {k:v for k, v in locals().iteritems() if v is not None}
+ params = {k:v for k, v in locals().items() if v is not None}
  response = requests.get(BASE_URL, params=params)
  response.raise_for_status()
- return json.loads(response.content)['results']
+ return response.json()['results']
 
 
