6668c8c823680aa37f9fe51a72c5752576606291
Christopher Toth <q.alpha@gmail.com>
2025-10-21 22:41:53 -0600

    Type-safe

--- itunes_search.py
+++ itunes_search.py
@@ -1,10 +1,24 @@
+from typing import Optional
+
 import requests
 
 BASE_URL = "https://itunes.apple.com/search"
 
 
-def search(term, country="US", media="all", entity=None, attribute=None, limit=50, lang="en_us", explicit="Yes"):
-    params = {k: v for k, v in locals().items() if v is not None}
-    response = requests.get(BASE_URL, params=params)
+def search(
+    term: str,
+    country: str = "US",
+    media: str = "all",
+    entity: Optional[str] = None,
+    attribute: Optional[str] = None,
+    limit: int = 50,
+    lang: str = "en_us",
+    explicit: str = "Yes",
+    session: Optional[requests.Session] = None,
+):
+    if session is None:
+        session = requests.Session()
+    params = {k: v for k, v in locals().items() if v is not None and k != "session"}
+    response = session.get(BASE_URL, params=params)
     response.raise_for_status()
     return response.json()["results"]
